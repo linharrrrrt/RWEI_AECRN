1cba96f9c515632df295a05be64f1ef4041372fd
EmeryLin <2272299218@qq.com>
2023-06-02 10:14:51 +0800

    Add files via upload

---
+++ AERCN/network_x8_attention.py
@@ -0,0 +1,104 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+# from SelfAttention import ScaledDotProductAttention 
+# from aspp import ASPP3D
+from attention import attention
+
+
+class Network_x8_attention(nn.Module):
+	'''
+	FCN architecture for scene coordiante regression.
+
+	The network predicts a 3d scene coordinates, the output is subsampled by a factor of 8 compared to the input.
+	'''
+
+	OUTPUT_SUBSAMPLE = 8
+
+	def __init__(self, mean, tiny):
+		'''
+		Constructor.
+		'''
+		super(Network_x8_attention, self).__init__()
+
+		self.conv1 = nn.Conv2d(1, 32, 3, 1, 1)
+		self.conv2 = nn.Conv2d(32, 64, 3, 2, 1)
+		self.conv3 = nn.Conv2d(64, 128, 3, 2, 1)
+		# self.conv3 = nn.Conv2d(64, 128, 3, 1, 1)
+		self.cbam1 = attention(gate_channels=128,reduction_ratio=16)
+		self.conv4 = nn.Conv2d(128, (256,128)[tiny], 3, 2, 1)
+		self.cbam2 = attention(gate_channels=(256,128)[tiny],reduction_ratio=16)
+
+		self.res1_conv1 = nn.Conv2d((256,128)[tiny], (256,128)[tiny], 3, 1, 1)
+		self.res1_conv2 = nn.Conv2d((256,128)[tiny], (256,128)[tiny], 1, 1, 0)
+		self.res1_conv3 = nn.Conv2d((256,128)[tiny], (256,128)[tiny], 3, 1, 1)
+
+		self.res2_conv1 = nn.Conv2d((256,128)[tiny], (512,128)[tiny], 3, 1, 1)
+		self.res2_conv2 = nn.Conv2d((512,128)[tiny], (512,128)[tiny], 1, 1, 0)
+		self.res2_conv3 = nn.Conv2d((512,128)[tiny], (512,128)[tiny], 3, 1, 1)
+
+		if not tiny:
+			self.res2_skip = nn.Conv2d(256, 512, 1, 1, 0)
+
+		self.res3_conv1 = nn.Conv2d((512,128)[tiny], (512,128)[tiny], 1, 1, 0)
+		self.res3_conv2 = nn.Conv2d((512,128)[tiny], (512,128)[tiny], 1, 1, 0)
+		self.res3_conv3 = nn.Conv2d((512,128)[tiny], (512,128)[tiny], 1, 1, 0)
+
+		self.fc1 = nn.Conv2d((512,128)[tiny], (512,128)[tiny], 1, 1, 0)
+		self.fc2 = nn.Conv2d((512,128)[tiny], (512,128)[tiny], 1, 1, 0)
+		self.fc3 = nn.Conv2d((512,128)[tiny], 3, 1, 1, 0)
+
+		# learned scene coordinates relative to a mean coordinate (e.g. center of the scene)
+		self.register_buffer('mean', torch.tensor(mean.size()).cuda())
+		self.mean = mean.clone()
+		self.tiny = tiny
+
+	def forward(self, inputs):
+		'''
+		Forward pass.
+
+		inputs -- 4D data tensor (BxCxHxW)
+		'''
+
+		x = inputs
+		x = F.relu(self.conv1(x))
+		x = F.relu(self.conv2(x))
+		x = F.relu(self.conv3(x))
+		xatt1 = self.cbam1(x)
+		x = x+xatt1
+		res = F.relu(self.conv4(x))
+		xatt2 = self.cbam2(res)
+		res = res+xatt2
+
+		x = F.relu(self.res1_conv1(res))
+		x = F.relu(self.res1_conv2(x))
+		x = F.relu(self.res1_conv3(x))
+		
+		res = res + x
+
+		x = F.relu(self.res2_conv1(res))
+		x = F.relu(self.res2_conv2(x))
+		x = F.relu(self.res2_conv3(x))
+
+		if not self.tiny:
+			res = self.res2_skip(res)
+
+		res = res + x
+
+		x = F.relu(self.res3_conv1(res))
+		x = F.relu(self.res3_conv2(x))
+		x = F.relu(self.res3_conv3(x))
+
+		res = res + x		
+
+		sc = F.relu(self.fc1(res))
+		sc = F.relu(self.fc2(sc))
+		sc = self.fc3(sc)
+		
+		sc[:, 0] += self.mean[0]
+		sc[:, 1] += self.mean[1]
+		sc[:, 2] += self.mean[2]
+
+		return sc
+
---
+++ AERCN/train_e2e_ecd.py
@@ -0,0 +1,153 @@
+import torch
+import torch.optim as optim
+
+import argparse
+import time
+import random
+
+import dsacstar
+
+from dataset import CamLocDataset, CamLocDataset_ecd
+
+from network_x8_attention import Network_x8_attention as Network
+
+
+
+parser = argparse.ArgumentParser(
+	description='Train scene coordinate regression in an end-to-end fashion.',
+	formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument('scene', help='name of a scene in the dataset folder')
+
+parser.add_argument('network_in', help='file name of a network initialized for the scene')
+
+parser.add_argument('network_out', help='output file name for the new network')
+
+parser.add_argument('--hypotheses', '-hyps', type=int, default=64, 
+	help='number of hypotheses, i.e. number of RANSAC iterations')
+
+parser.add_argument('--threshold', '-t', type=float, default=10, 
+	help='inlier threshold in pixels (RGB) or centimeters (RGB-D)')
+
+parser.add_argument('--inlieralpha', '-ia', type=float, default=100, 
+	help='alpha parameter of the soft inlier count; controls the softness of the hypotheses score distribution; lower means softer')
+
+parser.add_argument('--learningrate', '-lr', type=float, default=0.000001, 
+	help='learning rate')
+
+parser.add_argument('--iterations', '-it', type=int, default=100000, 
+	help='number of training iterations, i.e. network parameter updates')
+
+parser.add_argument('--weightrot', '-wr', type=float, default=1.0, 
+	help='weight of rotation part of pose loss')
+
+parser.add_argument('--weighttrans', '-wt', type=float, default=100.0, 
+	help='weight of translation part of pose loss')
+
+parser.add_argument('--softclamp', '-sc', type=float, default=100, 
+	help='robust square root loss after this threshold')
+
+parser.add_argument('--maxpixelerror', '-maxerrr', type=float, default=100, 
+	help='maximum reprojection (RGB, in px) or 3D distance (RGB-D, in cm) error when checking pose consistency towards all measurements; error is clamped to this value for stability')
+
+parser.add_argument('--mode', '-m', type=int, default=1, choices=[1,2],
+	help='test mode: 1 = RGB, 2 = RGB-D')
+
+parser.add_argument('--tiny', '-tiny', action='store_true',
+	help='Train a model with massively reduced capacity for a low memory footprint.')
+
+parser.add_argument('--session', '-sid', default='',
+	help='custom session name appended to output files. Useful to separate different runs of the program')
+
+opt = parser.parse_args()
+
+trainset = CamLocDataset_ecd("./datasets/" + opt.scene + "/train", mode=(0 if opt.mode < 2 else opt.mode), augment=False, aug_rotation=0, aug_scale_min=1, aug_scale_max=1) # use only photometric augmentation, not rotation and scaling
+trainset_loader = torch.utils.data.DataLoader(trainset, shuffle=True, num_workers=6)
+
+print("Found %d training images for %s." % (len(trainset), opt.scene))
+
+# load network
+network = Network(torch.zeros((3)), opt.tiny)
+network.load_state_dict(torch.load(opt.network_in))
+network = network.cuda()
+network.train()
+
+print("Successfully loaded %s." % opt.network_in)
+
+optimizer = optim.Adam(network.parameters(), lr=opt.learningrate)
+
+iteration = 0
+epochs = int(opt.iterations / len(trainset))
+
+# keep track of training progress
+train_log = open('log_e2e_%s_%s.txt' % (opt.scene, opt.session), 'w', 1)
+
+training_start = time.time()
+
+for epoch in range(epochs):	
+
+	print("=== Epoch: %7d ======================================" % epoch)
+
+	# for image, pose, camera_coordinates, focal_length, file, mask in trainset_loader:
+	for image, pose, camera_coordinates, focal_length, file in trainset_loader:
+
+		start_time = time.time()
+
+		focal_length = float(focal_length[0])
+		pose = pose[0]
+
+		# predict scene coordinates
+		scene_coordinates = network(image.cuda())
+		scene_coordinate_gradients = torch.zeros(scene_coordinates.size())
+
+		if opt.mode == 2:
+			# RGB-D mode
+			loss = dsacstar.backward_rgbd(
+				scene_coordinates.cpu(), 
+				camera_coordinates,
+				scene_coordinate_gradients,
+				pose, 
+				opt.hypotheses, 
+				opt.threshold,
+				opt.weightrot,
+				opt.weighttrans,
+				opt.softclamp,
+				opt.inlieralpha,
+				opt.maxpixelerror,
+				random.randint(0,1000000)) # used to initialize random number generator in C++
+
+		else:
+			# RGB mode
+			loss = dsacstar.backward_rgb(
+				scene_coordinates.cpu(), 
+				scene_coordinate_gradients,
+				pose, 
+				opt.hypotheses, 
+				opt.threshold,
+				focal_length, 
+				float(image.size(3) / 2), #principal point assumed in image center
+				float(image.size(2) / 2),
+				opt.weightrot,
+				opt.weighttrans,
+				opt.softclamp,
+				opt.inlieralpha,
+				opt.maxpixelerror,
+				network.OUTPUT_SUBSAMPLE,
+				random.randint(0,1000000)) # used to initialize random number generator in C++
+
+		# update network parameters
+		torch.autograd.backward((scene_coordinates), (scene_coordinate_gradients.cuda()))
+		optimizer.step()
+		optimizer.zero_grad()
+		
+		end_time = time.time()-start_time
+		print('Iteration: %6d, Loss: %.2f, Time: %.2fs \n' % (iteration, loss, end_time), flush=True)
+
+		train_log.write('%d %f\n' % (iteration, loss))
+		iteration = iteration + 1
+
+	print('Saving snapshot of the network to %s.' % opt.network_out)
+	torch.save(network.state_dict(), "networkout/"+opt.network_out+str(epoch))
+
+print('Done without errors. Time: %.1f minutes.' % ((time.time() - training_start) / 60))
+train_log.close()
